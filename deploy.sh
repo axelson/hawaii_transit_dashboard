ddeb90defb82df6676830f14fb5baa9c0ac3f483
Jason Axelson <jason.axelson@gmail.com>
2014-12-10 23:16:14 -1000

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+git diff --exit-code >/dev/null
+if [ $? -ne 0 ]; then
+    echo "Requires a clean git directory"
+    exit 1
+fi
+
+rm -r _site/
+bundle exec jekyll build --config _deploy_config.yml
+cp -r _site/ ../hawaii-transit-site-bak/
+git checkout gh-pages
+rm -r *
+mv ../hawaii-transit-site-bak/* .
+git add --all .
